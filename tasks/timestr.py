fb356874e06545dbe81b9f05f616507de1df5f39
anton <LolAnthonyo@yandex.ru>
2023-08-27 01:37:15 +0300

    fixing actions

--- tasks/timestr.py
+++ tasks/timestr.py
@@ -6,11 +6,6 @@ __all__ = (
 def seconds_to_str(seconds: int) -> str:
     """
     Функция должна вернуть текстовое представление времени
-    20 -> 20s
-    60 -> 01m00s
-    65 -> 01m05s
-    3700 -> 01h01m40s
-    93600 -> 01d02h00m00s
     """
     minutes = int(seconds / 60 % 60)
     hours = int(seconds / 60 / 60 % 24)
